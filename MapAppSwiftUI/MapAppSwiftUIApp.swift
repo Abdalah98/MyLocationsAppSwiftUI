f44f6aaf113cb24f77d5076f7e341be4567710cb
Abdalah98 <abdalahomar48@gmail.com>
2022-12-28 09:35:21 +0200

    first commit

---
+++ MapAppSwiftUI/MapAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppSwiftUIApp.swift
+//  MapAppSwiftUI
+//
+//  Created by Abdallah on 28/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
